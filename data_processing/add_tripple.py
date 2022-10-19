d4702fff3e433e96e69e0db36b8e9dc3b33ad313
Samuel Van Ackere <samuel.vanackere@vlaanderen.be>
2022-10-19 15:38:14 +0200

    plot regression models

--- data_processing/add_tripple.py
+++ data_processing/add_tripple.py
@@ -1,20 +1,20 @@
 from rdflib import Graph, URIRef, Literal, BNode
 from rdflib.namespace import FOAF, RDF
 
-g = Graph()
-g.bind("foaf", FOAF)
 
-bob = URIRef("http://example.org/people/Bob")
-linda = BNode()  # a GUID is generated
+def add_tripple_to_graph(graph_input) #, s_input, p_input, o_input
+    g = graph_input
+    g.bind("foaf", FOAF)
+    bob = URIRef("http://example.org/people/Bob")
+    linda = BNode()  # a GUID is generated
+    name = Literal("Bob")
+    age = Literal(24)
+    g.add((bob, RDF.type, FOAF.Person))
+    g.add((bob, FOAF.name, name))
+    g.add((bob, FOAF.age, age))
+    g.add((bob, FOAF.knows, linda))
+    g.add((linda, RDF.type, FOAF.Person))
+    g.add((linda, FOAF.name, Literal("Linda")))
+    print(g.serialize())
+    return g
 
-name = Literal("Bob")
-age = Literal(24)
-
-g.add((bob, RDF.type, FOAF.Person))
-g.add((bob, FOAF.name, name))
-g.add((bob, FOAF.age, age))
-g.add((bob, FOAF.knows, linda))
-g.add((linda, RDF.type, FOAF.Person))
-g.add((linda, FOAF.name, Literal("Linda")))
-
-print(g.serialize())
---
+++ data_processing/add_tripple_example.py
@@ -0,0 +1,20 @@
+from rdflib import Graph, URIRef, Literal, BNode
+from rdflib.namespace import FOAF, RDF
+
+g = Graph()
+g.bind("foaf", FOAF)
+
+bob = URIRef("http://example.org/people/Bob")
+linda = BNode()  # a GUID is generated
+
+name = Literal("Bob")
+age = Literal(24)
+
+g.add((bob, RDF.type, FOAF.Person))
+g.add((bob, FOAF.name, name))
+g.add((bob, FOAF.age, age))
+g.add((bob, FOAF.knows, linda))
+g.add((linda, RDF.type, FOAF.Person))
+g.add((linda, FOAF.name, Literal("Linda")))
+
+print(g.serialize())
