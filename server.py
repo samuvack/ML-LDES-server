845b5e110a81210efe56f3097a481cd6f0ae01aa
Samuel Van Ackere <samuel.vanackere@vlaanderen.be>
2022-10-21 09:04:04 +0200

    update server POST/GET

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import data_processing.ttl2jsonld
 import data_processing.crawled
 import ml_tests.regression_input
 #import ml_tests.add_tripple
-
+from rdflib import Graph, plugin
 from functools import lru_cache
 
 
@@ -25,13 +25,12 @@ class One(object):
 
 One().set_a('There is no POST request received yet in the ml server')
 
-
 class webserverHandler(BaseHTTPRequestHandler):
     """docstring for webserverHandler"""
 
     def do_GET(self):
         try:
-            if self.path.endswith("/output_rdf"):
+            if self.path.endswith("/output"):
                 self.send_response(200)
                 self.send_header('Content-Type', 'text/html')
                 self.end_headers()
@@ -56,66 +55,74 @@ class webserverHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.end_headers()
-            ctype, pdict = cgi.parse_header(self.headers.get('Content-Type'))
+            if self.path.endswith("/input"):
+                self.send_response(200)
+                self.send_header('Content-Type', 'text/html')
+                self.end_headers()
+                ctype, pdict = cgi.parse_header(self.headers.get('Content-Type'))
 
 
-            content_len = int(self.headers.get('Content-length'))
-            post_body = self.rfile.read(content_len)
-            #pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
-            #pdict['CONTENT-LENGTH'] = content_len
-            print('POST request input:')
-            print(post_body)
+                content_len = int(self.headers.get('Content-length'))
+                post_body = self.rfile.read(content_len)
+                #pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+                #pdict['CONTENT-LENGTH'] = content_len
+                print('POST request input:')
+                print(post_body)
 
-            
-            testrdf = """
-            @prefix dcterms: <http://purl.org/dc/terms/> .
-            <http://example.org/about>
-            dcterms:temperature "10"@en ;
-            dcterms:salinity "20"@en .
-            """
+                
+                testrdf = """
+                @prefix dcterms: <http://purl.org/dc/terms/> .
+                <http://example.org/about>
+                dcterms:temperature "10"@en ;
+                dcterms:salinity "20"@en .
+                """
 
-            """
+                ##CONVERSION TO JSON-LD
+                print('conversion to json-ld:')
+                #print(data_processing.ttl2jsonld.convert_rdf_2_jsonld(post_body))
+                input = data_processing.ttl2jsonld.convert_rdf_2_jsonld(post_body)
+                
+                
+                g = Graph().parse(data=input, format='json-ld')
+                salinity = data_processing.crawled.crawl_parameter('http://purl.org/dc/terms/salinity', g)
+                print('crawled salinity is :' + salinity)
 
-            ##CONVERSION TO JSON-LD
-            print('conversion to json-ld:')
-            print(data_processing.ttl2jsonld.convert_rdf_2_jsonld(post_body))
+                # LINEAR REGRESSION
+                print('--running regression models--')
+                print('estimated temperature based on linear regression:')
+                print(ml_tests.regression_input.run_linear_model(int(salinity)))
+                temp_ml_lin = str(ml_tests.regression_input.run_linear_model(int(salinity)))
 
-            graph = data_processing.ttl2jsonld.convert_rdf_2_jsonld(post_body)
-            g = Graph().parse(data=testrdf, format='n3')
-            print(g)
-            salinity = data_processing.crawled.crawl_parameter('http://purl.org/dc/terms/salinity', g)
-            print('crawled salinity is :' + salinity)
+                # MULTIPLE LINEAR REGRESSION
+                print('estimated temperature based on linear regression:')
+                print(ml_tests.regression_input.run_linear_model(int(salinity)))
+                temp_ml_lin = str(ml_tests.regression_input.run_linear_model(int(salinity)))
 
+                # POLYNOMIAL REGRESSION
+                print('estimated temperature based on linear regression:')
+                print(ml_tests.regression_input.run_linear_model(int(salinity)))
+                temp_ml_lin = str(ml_tests.regression_input.run_linear_model(int(salinity)))
 
-            print('--Nu gaan we Linear model runnen--')
-            print('Schatting temperatuur volgens linear regression:')
-            print(ml_tests.regression_input.run_linear_model(salinity))
-            temp_ml_lin = str(ml_tests.regression_input.run_linear_model(salinity))
+                
+                One().set_a(temp_ml_lin)
+                
+                output = ""
+                output += '<html><body>&#161Hola <a href="/hello">Back to Hello</a>'
+                output += '<form method="POST" enctype="multipart/form-data" action="/hello"><h2> What would you like me to say?</h2><input name="message" type="text" /><input type="submit" value="Submit" /></form>'
+                output += '</body></html>'
 
-            One().set_a(temp_ml_lin)
-            
-            """
-
-            output = ""
-            output += '<html><body>&#161Hola <a href="/hello">Back to Hello</a>'
-            output += '<form method="POST" enctype="multipart/form-data" action="/hello"><h2> What would you like me to say?</h2><input name="message" type="text" /><input type="submit" value="Submit" /></form>'
-            output += '</body></html>'
-
-            ##ADD PREDICTION TO GRAPH
-            test = postbody
-            self.wfile.write(str(output).encode())
+                ##ADD PREDICTION TO GRAPH
+                test = output
+                self.wfile.write(output.encode())
+                return
         except:
-            self.send_error(404, "{}".format(sys.exc_info()[0]))
+            self.send_error(404, "{}".format(sys.exc_info()[1]))
             #print(sys.exc_info())
 
 def main():
     try:
         port = 8000
         server = HTTPServer(('', port), webserverHandler)
-
         print("Web server running on port %s" % port)
         server.serve_forever()
 
