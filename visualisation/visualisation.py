1ec4a7375b6940797c2d7a7454b5e20041f01975
Samuel Van Ackere <samuel.vanackere@vlaanderen.be>
2022-11-29 16:48:06 +0100

    edit core structure

--- visualisation/visualisation.py
+++ visualisation/visualisation.py
@@ -1,13 +1,11 @@
 import matplotlib.pyplot as plt
 
-
 # LINEAR REGRESSION MODEL
 # Draw figure of Machine Learning Model
 
 plt.scatter(X_test, y_test, color='r')
 plt.plot(X_test, y_pred, color='g')
 plt.show()
-
 
 # MULTIPLE LINEAR REGRESSION
 # Finally draw figure of Multiple Linear Regression Model
@@ -16,7 +14,7 @@ plt.scatter(X_train, y_train, s=100)
 
 # sort the values of x before line plot
 sort_axis = operator.itemgetter(0)
-sorted_zip = sorted(zip(X_train,y_pred), key=sort_axis)
+sorted_zip = sorted(zip(X_train, y_pred), key=sort_axis)
 X_test, y_pred = zip(*sorted_zip)
 plt.plot(X_train, y_train, color='g')
 plt.show()
@@ -26,8 +24,7 @@ plt.show()
 plt.scatter(X_train, y_train, s=50)
 # sort the values of x before line plot
 sort_axis = operator.itemgetter(0)
-sorted_zip = sorted(zip(X_train,polynomial_predict), key=sort_axis)
+sorted_zip = sorted(zip(X_train, polynomial_predict), key=sort_axis)
 X_train, polynomial_predict = zip(*sorted_zip)
 plt.plot(X_train, polynomial_predict, color='m')
 plt.show()
-
