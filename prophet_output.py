963d2d1987c5ec5844ae93ac7a29b1bea840d607
Samuel Van Ackere <samuel.vanackere@vlaanderen.be>
2022-11-29 16:17:32 +0100

    Prophet ml pipeline

--- prophet_output.py
+++ prophet_output.py
@@ -6,30 +6,25 @@ import pandas.io.sql as sqlio
 from sklearn.metrics import r2_score
 
 
-def prophet_output(dat)
+def prophet_output(dat):
+    """Runs forecasting model Prophet
 
+    Args:
+        dat (dataframe): ds, y columns in dataframe give info about time series
+
+    Returns:
+        dataframe: forecasting output in the form of a dataframe
+    """
     dat.drop_duplicates(subset=['ds'])
+    last_value = dat['ds'].iat[-1]
     m = Prophet()
-    m.fit(dat)  # df is a pandas.DataFrame with 'y' and 'ds' columns
+    # df is a pandas.DataFrame with 'y' and 'ds' columns
+    m.fit(dat)
     future = m.make_future_dataframe(periods=2)
-    print(future.tail())
-    m.predict(future)
-
     forecast = m.predict(future)
-    forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
-    print(forecast)
-
-    ax = forecast.plot('ds', 'yhat')
-    forecast.plot('ds', 'yhat_lower', ax=ax)
-    forecast.plot('ds', 'yhat_upper', ax=ax)
-    plt.show()
+    values_forecasting = forecast[forecast['ds']
+                                  > last_value]
+    return values_forecasting
 
 
-
-
-    ax3=m.plot(forecast)
-    plt.show()
-
-    ax2 = m.plot_components(forecast)
-    plt.show()
 
